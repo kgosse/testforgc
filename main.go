4ba7900f4a5c45af78c54837f6d273bff7a336e7
Kévin Gossé <gossekevin1@gmail.com>
2019-03-28 08:18:31 +0100

    print 10 names

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"regexp"
 )
+
+var re = regexp.MustCompile(`^([a-zA-Z]+), ([a-zA-Z]+) -- .+\n$`)
 
 func main() {
 	f := flag.String("f", "test-input-file.list", "the input file")
@@ -13,6 +19,30 @@ func main() {
 	file, err := os.Open(*f)
 	defer file.Close()
 	handleError(err)
+
+	reader := bufio.NewReader(file)
+
+	var line string
+	cpt := 0
+	for {
+		line, err = reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		handleError(err)
+
+		if re.MatchString(line) {
+			n := re.FindStringSubmatch(line)
+			l, f := n[1], n[2]
+			fmt.Printf("%s, %s\n", l, f)
+			cpt++
+		}
+
+		if cpt == 10 {
+			break
+		}
+
+	}
 }
 
 func handleError(e error) {
