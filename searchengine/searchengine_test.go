639d94e214136882efa154088e67d96592db3a3a
Kévin Gossé <gossekevin1@gmail.com>
2019-03-28 08:24:58 +0100

    add structures in searchengine

--- searchengine/searchengine_test.go
+++ searchengine/searchengine_test.go
@@ -1,1 +1,13 @@
 package searchengine_test
+
+// Item is a single element
+type Item struct {
+	Name   string
+	Weight int
+}
+
+// List structures the data
+type List struct {
+	Indexes []string
+	Set     map[string]int
+}
