68772bcf4337159532853762820cd50f80771464
Kévin Gossé <gossekevin1@gmail.com>
2019-03-28 08:37:38 +0100

    searchengine_test: add function signatures

--- searchengine/searchengine_test.go
+++ searchengine/searchengine_test.go
@@ -1,34 +1,15 @@
 package searchengine_test
 
-// Item is a single element
-type Item struct {
-	Name   string
-	Weight int
+import "testing"
+
+func TestNewList(t *testing.T) {
 }
 
-// List structures the data
-type List struct {
-	Indexes []string
-	Set     map[string]int
+func TestAdd(t *testing.T) {
 }
 
-// NewList creates an new empty List
-func NewList() List {
-	return List{}
+func TestGetUniqueElementsCount(t *testing.T) {
 }
 
-// Add inserts a new element into the list
-func (l List) Add(s string) {
-}
-
-// GetUniqueElementsCount returns the number of uniq elements
-func (l List) GetUniqueElementsCount() int {
-	ct := 0
-
-	return ct
-}
-
-// GetTopElements returns the top n elements (by weight)
-func (l List) GetTopElements(n int) []Item {
-	return nil
+func TestGetTopElements(t *testing.T) {
 }
