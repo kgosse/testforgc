2ee74f20660aaded677d2875d9ea21d2a65f66f4
Kévin Gossé <gossekevin1@gmail.com>
2019-03-28 08:32:45 +0100

    searchengine: add function signatures

--- searchengine/searchengine_test.go
+++ searchengine/searchengine_test.go
@@ -11,3 +11,24 @@ type List struct {
 	Indexes []string
 	Set     map[string]int
 }
+
+// NewList creates an new empty List
+func NewList() List {
+	return List{}
+}
+
+// Add inserts a new element into the list
+func (l List) Add(s string) {
+}
+
+// GetUniqueElementsCount returns the number of uniq elements
+func (l List) GetUniqueElementsCount() int {
+	ct := 0
+
+	return ct
+}
+
+// GetTopElements returns the top n elements (by weight)
+func (l List) GetTopElements(n int) []Item {
+	return nil
+}
