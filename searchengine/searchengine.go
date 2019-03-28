4e85a1857e1c30b24c819c9f2dfc57021cf5c578
Kévin Gossé <gossekevin1@gmail.com>
2019-03-28 08:35:14 +0100

    move content of searchengine_test to searchengine

--- searchengine/searchengine.go
+++ searchengine/searchengine.go
@@ -1,1 +1,34 @@
 package searchengine
+
+// Item is a single element
+type Item struct {
+	Name   string
+	Weight int
+}
+
+// List structures the data
+type List struct {
+	Indexes []string
+	Set     map[string]int
+}
+
+// NewList creates an new empty List
+func NewList() List {
+	return List{}
+}
+
+// Add inserts a new element into the list
+func (l List) Add(s string) {
+}
+
+// GetUniqueElementsCount returns the number of uniq elements
+func (l List) GetUniqueElementsCount() int {
+	ct := 0
+
+	return ct
+}
+
+// GetTopElements returns the top n elements (by weight)
+func (l List) GetTopElements(n int) []Item {
+	return nil
+}
